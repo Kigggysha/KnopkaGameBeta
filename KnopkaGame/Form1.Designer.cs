6cb3803facbc86358feb61dba48788ed6f1694b2
Kirill <kirillbel2004@gmail.com>
2022-11-18 10:42:07 +0300

    Добавил зваки кнопкам

--- KnopkaGame/Form1.Designer.cs
+++ KnopkaGame/Form1.Designer.cs
@@ -30,7 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Start = new System.Windows.Forms.PictureBox();
+            this.Continue = new System.Windows.Forms.PictureBox();
+            this.Settings = new System.Windows.Forms.PictureBox();
+            this.Exit = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.Start)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Continue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Settings)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Exit)).BeginInit();
             this.SuspendLayout();
             // 
             // Start
@@ -38,15 +44,66 @@
             this.Start.BackColor = System.Drawing.Color.Transparent;
             this.Start.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Start.BackgroundImage")));
             this.Start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.Start.Location = new System.Drawing.Point(70, 80);
+            this.Start.Location = new System.Drawing.Point(65, 65);
             this.Start.Name = "Start";
-            this.Start.Size = new System.Drawing.Size(333, 89);
+            this.Start.Size = new System.Drawing.Size(435, 115);
             this.Start.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Start.TabIndex = 0;
             this.Start.TabStop = false;
             this.Start.Click += new System.EventHandler(this.Start_Click);
+            this.Start.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Start_MouseDown);
+            this.Start.MouseEnter += new System.EventHandler(this.Start_MouseHover);
             this.Start.MouseLeave += new System.EventHandler(this.Start_MouseLeave);
-            this.Start.MouseHover += new System.EventHandler(this.Start_MouseHover);
+            this.Start.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Start_MouseUp);
+            // 
+            // Continue
+            // 
+            this.Continue.BackColor = System.Drawing.Color.Transparent;
+            this.Continue.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Continue.BackgroundImage")));
+            this.Continue.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Continue.Location = new System.Drawing.Point(65, 200);
+            this.Continue.Name = "Continue";
+            this.Continue.Size = new System.Drawing.Size(435, 115);
+            this.Continue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Continue.TabIndex = 1;
+            this.Continue.TabStop = false;
+            this.Continue.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Continue_MouseDown);
+            this.Continue.MouseEnter += new System.EventHandler(this.Continue_MouseEnter);
+            this.Continue.MouseLeave += new System.EventHandler(this.Continue_MouseLeave);
+            this.Continue.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Continue_MouseUp);
+            // 
+            // Settings
+            // 
+            this.Settings.BackColor = System.Drawing.Color.Transparent;
+            this.Settings.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Settings.BackgroundImage")));
+            this.Settings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Settings.Location = new System.Drawing.Point(65, 335);
+            this.Settings.Name = "Settings";
+            this.Settings.Size = new System.Drawing.Size(435, 115);
+            this.Settings.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Settings.TabIndex = 2;
+            this.Settings.TabStop = false;
+            this.Settings.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Settings_MouseDown);
+            this.Settings.MouseEnter += new System.EventHandler(this.Settings_MouseEnter);
+            this.Settings.MouseLeave += new System.EventHandler(this.Settings_MouseLeave);
+            this.Settings.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Settings_MouseUp);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.Transparent;
+            this.Exit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Exit.BackgroundImage")));
+            this.Exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Exit.Location = new System.Drawing.Point(65, 475);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(435, 115);
+            this.Exit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Exit.TabIndex = 3;
+            this.Exit.TabStop = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            this.Exit.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Exit_MouseDown);
+            this.Exit.MouseEnter += new System.EventHandler(this.Exit_MouseEnter);
+            this.Exit.MouseLeave += new System.EventHandler(this.Exit_MouseLeave);
+            this.Exit.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Exit_MouseUp);
             // 
             // Form1
             // 
@@ -56,11 +113,17 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1264, 701);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Settings);
+            this.Controls.Add(this.Continue);
             this.Controls.Add(this.Start);
             this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "KnopkaGame";
             ((System.ComponentModel.ISupportInitialize)(this.Start)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Continue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Settings)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Exit)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,5 +131,8 @@
         #endregion
 
         private PictureBox Start;
+        private PictureBox Continue;
+        private PictureBox Settings;
+        private PictureBox Exit;
     }
 }
--- KnopkaGame/Form1.cs
+++ KnopkaGame/Form1.cs
@@ -1,27 +1,119 @@
+using System.IO;
+using System.Numerics;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace KnopkaGame
 {
     public partial class Form1 : Form
     {
+        //play sound of selected button
+        System.Media.SoundPlayer ButtonSelect = new System.Media.SoundPlayer("../KnopkaGame/Sounds/ButtonSelect.wav");
+        //play sound of pushed button
+        System.Media.SoundPlayer ButtonPush = new System.Media.SoundPlayer("../KnopkaGame/Sounds/ButtonPush.wav");
         public Form1()
         {
             InitializeComponent();
             Start.BackColor = Color.Transparent;
         }
+        //Start button click function
         private void Start_Click(object sender, EventArgs e)
         {
-
+            //sound player
+            ButtonPush.Play();
         }
         //Change picture when mouse located on start button
         private void Start_MouseHover(object sender, EventArgs e)
         {
-            Start.BackgroundImage = Image.FromFile("D:/C#/KnopkaGameBeta/KnopkaGame/KnopkaGame/Textures/StartButtonSelect.png");
+            Start.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/StartButtonSelect.png");
+            ButtonSelect.Play();
         }
         //Change picture when mouse move out start button
         private void Start_MouseLeave(object sender, EventArgs e)
         {
-            Start.BackgroundImage = Image.FromFile("D:/C#/KnopkaGameBeta/KnopkaGame/KnopkaGame/Textures/StartButton.png");
+            Start.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/StartButton.png");
+        }
+
+        private void Continue_MouseEnter(object sender, EventArgs e)
+        {
+            Continue.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/ContinueSelect.png");
+            ButtonSelect.Play();
+        }
+
+        private void Continue_MouseLeave(object sender, EventArgs e)
+        {
+            Continue.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/Continue.png");
+        }
+
+        private void Settings_MouseEnter(object sender, EventArgs e)
+        {
+            Settings.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/SettingsSelect.png");
+            ButtonSelect.Play();
+        }
+
+        private void Settings_MouseLeave(object sender, EventArgs e)
+        {
+            Settings.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/Settings.png");
+        }
+
+        private void Exit_MouseEnter(object sender, EventArgs e)
+        {
+            Exit.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/ExitSelect.png");
+            ButtonSelect.Play();
+        }
+
+        private void Exit_MouseLeave(object sender, EventArgs e)
+        {
+            Exit.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/Exit.png");
+        }
+
+        private void Start_MouseUp(object sender, MouseEventArgs e)
+        {
+            Start.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/StartButtonSelect.png");
+        }
+
+        private void Start_MouseDown(object sender, MouseEventArgs e)
+        {
+            Start.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/StartButtonPush.png");
+        }
+
+        private void Continue_MouseDown(object sender, MouseEventArgs e)
+        {
+            Continue.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/ContinuePush.png");
+            ButtonPush.Play();
+        }
+
+        private void Continue_MouseUp(object sender, MouseEventArgs e)
+        {
+            Continue.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/ContinueSelect.png");
+        }
+
+        private void Settings_MouseDown(object sender, MouseEventArgs e)
+        {
+            Settings.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/SettingsPush.png");
+            ButtonPush.Play();
+        }
+
+        private void Settings_MouseUp(object sender, MouseEventArgs e)
+        {
+            Settings.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/SettingsSelect.png");
+        }
+
+        private void Exit_MouseDown(object sender, MouseEventArgs e)
+        {
+            Exit.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/ExitPush.png");
+        }
+
+        private void Exit_MouseUp(object sender, MouseEventArgs e)
+        {
+            Exit.BackgroundImage = Image.FromFile("../KnopkaGame/Textures/ExitSelect.png");
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            ButtonPush.Play();
+            Thread.Sleep(400);
+            this.Close();
         }
     }
 }
